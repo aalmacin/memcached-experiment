36cb1855025cc9830696c42178817d6a750ee4f4
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2022-12-09 15:14:02 -0600

    Add stats

--- src/main/java/MemcachedExperiment.java
+++ src/main/java/MemcachedExperiment.java
@@ -6,6 +6,8 @@ import net.rubyeye.xmemcached.exception.MemcachedException;
 import net.rubyeye.xmemcached.utils.AddrUtil;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 public class MemcachedExperiment {
@@ -20,8 +22,17 @@ public class MemcachedExperiment {
 
         MemcachedClient mc = builder.build();
 
-        mc.set("foo", 0, "bar");
+        mc.set("foo", 300, "bar");
+        mc.set("foo1", 300, "fizz");
+        mc.set("foo2", 300, "baz");
+
         String val = mc.get("foo");
-        System.out.println(val);
+
+        System.out.println("Added value: " + val);
+
+        Map<InetSocketAddress, Map<String, String>> stats = mc.getStatsByItem("items");
+        stats.forEach((s, i) -> {
+            i.keySet().forEach(c -> System.out.println(c.toString()));
+        });
     }
 }
